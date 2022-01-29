6c49574165fb9224806423de2ca388fdb917bc2d
Park <andy3638@naver.com>
2022-01-29 14:06:33 +0900

    데스크탑으로 옮기려고 커밋

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import "./App.css";
-import { Button, Form } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 function App() {
-    const [아이디, 아이디변경] = useState(0);
-    const [비밀번호, 비밀번호변경] = useState(0);
+  const [isRedirect, setIsRedirect] = useState(false);
+  const { register, handleSubmit } = useForm();
 
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => axios.post("localhost:3001/auth/join");
+  const onSubmit = async (data) => {
+    try {
+      await axios.post("http://localhost:3001/practice", data);
+      setIsRedirect(true);
+      console.log("채굴 클라이언트 성공");
+    } catch (err) {
+      console.log("채굴 클라이언트 실패");
+    }
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("email")} />
-                <input {...register("password")} />
-                <input type="submit" />
-            </form>
-            <Button variant="primary" type="submit">
-                채굴 시작
-            </Button>
-        </div>
-    );
+  return (
+    <div>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <h1>프로젝트 뿅</h1>
+        <Link to="/1blocks">블럭보기</Link> |{" "}
+        <Link to="/1expenses">그냥 만들어봄</Link>
+        <br></br>
+        <Link to="/join">회원가입</Link> | <Link to="/login">로그인</Link>
+        <br></br>
+        <Link to="/apis">Apis</Link>
+      </nav>
+
+      <h2>채굴해보기</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("number")} placeholder="채굴 인원" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
